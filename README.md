1b5adcb4395c5349365d57d94e97939eedd9b749
James Harton <james@harton.nz>
2024-09-13 15:25:11 +1200

    chore: first pass.

---
+++ README.md
@@ -0,0 +1,3 @@
+# lapce-elixir
+
+Lapce plugin for the [Elixir](https://elixir-lang.org/) programming language.
--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,77 @@
-// Deny usage of print and eprint as it won't have same result
-// in WASI as if doing in standard program, you must really know
-// what you are doing to disable that lint (and you don't know)
-#![deny(clippy::print_stdout)]
-#![deny(clippy::print_stderr)]
+use std::{fs::File, path::PathBuf};
 
 use anyhow::Result;
+use flate2::read::GzDecoder;
 use lapce_plugin::{
     psp_types::{
-        lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url, MessageType},
+        lsp_types::{request::Initialize, DocumentFilter, InitializeParams, MessageType, Url},
         Request,
     },
-    register_plugin, LapcePlugin, VoltEnvironment, PLUGIN_RPC,
+    register_plugin, Http, LapcePlugin, PLUGIN_RPC,
 };
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Default)]
 struct State {}
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PluginInfo {
+    arch: String,
+    os: String,
+    configuration: Configuration,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Configuration {
+    language_id: String,
+    options: Option<Value>,
+}
+
 register_plugin!(State);
 
 fn initialize(params: InitializeParams) -> Result<()> {
-    let document_selector: DocumentSelector = vec![DocumentFilter {
-        // lsp language id
-        language: Some(String::from("language_id")),
-        // glob pattern
-        pattern: Some(String::from("**/*.{ext1,ext2}")),
-        // like file:
-        scheme: None,
-    }];
-    let mut server_args = vec![];
-
-    // Check for user specified LSP server path
-    // ```
-    // [lapce-plugin-name.lsp]
-    // serverPath = "[path or filename]"
-    // serverArgs = ["--arg1", "--arg2"]
-    // ```
-    if let Some(options) = params.initialization_options.as_ref() {
-        if let Some(lsp) = options.get("lsp") {
-            if let Some(args) = lsp.get("serverArgs") {
-                if let Some(args) = args.as_array() {
-                    if !args.is_empty() {
-                        server_args = vec![];
-                    }
-                    for arg in args {
-                        if let Some(arg) = arg.as_str() {
-                            server_args.push(arg.to_string());
-                        }
-                    }
-                }
+    let server_path = params
+        .initialization_options
+        .as_ref()
+        .and_then(|options| options.get("serverPath"))
+        .and_then(|server_path| server_path.as_str())
+        .and_then(|server_path| {
+            if !server_path.is_empty() {
+                Some(server_path)
+            } else {
+                None
             }
+        });
 
-            if let Some(server_path) = lsp.get("serverPath") {
-                if let Some(server_path) = server_path.as_str() {
-                    if !server_path.is_empty() {
-                        let server_uri = Url::parse(&format!("urn:{}", server_path))?;
-                        PLUGIN_RPC.start_lsp(
-                            server_uri,
-                            server_args,
-                            document_selector,
-                            params.initialization_options,
-                        );
-                        return Ok(());
-                    }
-                }
-            }
-        }
+    if let Some(server_path) = server_path {
+        // TODO: once nightly is released, update plugin api to use request version of start lsp
+        // that will inform us of the error
+        PLUGIN_RPC.start_lsp(
+            Url::parse(&format!("urn:{}", server_path))?,
+            Vec::new(),
+            vec![DocumentFilter {
+                language: Some("elixir".to_string()),
+                scheme: None,
+                pattern: None,
+            }],
+            params.initialization_options,
+        );
+        return Ok(());
     }
 
-    // Architecture check
-    let _ = match VoltEnvironment::architecture().as_deref() {
-        Ok("x86_64") => "x86_64",
-        Ok("aarch64") => "aarch64",
-        _ => return Ok(()),
-    };
-
-    // OS check
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("macos") => "macos",
-        Ok("linux") => "linux",
-        Ok("windows") => "windows",
-        _ => return Ok(()),
-    };
-
-    // Download URL
-    // let _ = format!("https://github.com/<name>/<project>/releases/download/<version>/{filename}");
-
-    // see lapce_plugin::Http for available API to download files
-
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("windows") => {
-            format!("{}.exe", "[filename]")
-        }
-        _ => "[filename]".to_string(),
-    };
-
-    // Plugin working directory
-    let volt_uri = VoltEnvironment::uri()?;
-    let server_uri = Url::parse(&volt_uri)?.join("[filename]")?;
-
-    // if you want to use server from PATH
-    // let server_uri = Url::parse(&format!("urn:{filename}"))?;
-
-    // Available language IDs
-    // https://github.com/lapce/lapce/blob/HEAD/lapce-proxy/src/buffer.rs#L173
+    let volt_uri = std::env::var("VOLT_URI")?;
+    let server_path = Url::parse(&volt_uri)?.join("elixir-ls")?;
     PLUGIN_RPC.start_lsp(
-        server_uri,
-        server_args,
-        document_selector,
+        server_path,
+        Vec::new(),
+        vec![DocumentFilter {
+            language: Some("elixir".to_string()),
+            scheme: None,
+            pattern: None,
+        }],
         params.initialization_options,
     );
-
     Ok(())
 }
 
@@ -121,7 +82,7 @@ impl LapcePlugin for State {
             Initialize::METHOD => {
                 let params: InitializeParams = serde_json::from_value(params).unwrap();
                 if let Err(e) = initialize(params) {
-                    PLUGIN_RPC.window_show_message(MessageType::ERROR, format!("plugin returned with error: {e}"))
+                    PLUGIN_RPC.stderr(&format!("plugin returned with error: {e}"))
                 }
             }
             _ => {}
